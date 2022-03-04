9e2f04a95e5b8e69f1e239fcd6938c97b1ddf1e0
kumudinidesale <kdesale1211@gmail.com>
2022-03-04 19:01:34 +0530

    added:Check Regex For Email Sample Logic

--- src/Java/com/blz/UserRegistration.java
+++ src/Java/com/blz/UserRegistration.java
@@ -1,5 +1,5 @@
 /*@Purpose :As a User need to follow pre defined Password Rules
- *Rule4 – Has Special Character -All rules must be passed
+ *Should clear all Email samples provided separately
  *@File : User Registration Using Regex
  *@Author : Kumudini Khairnar.
  */
@@ -17,25 +17,20 @@ public class UserRegistration {
 	 * 
 	 * @param : Name, Regex, Matches
 	 */
-	public boolean passwrdRule(String inputStr) {
-		String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$";
-		Pattern regex = Pattern.compile(passwordPattern);
-		Matcher passwordMatcher = regex.matcher(inputStr);
-		if (passwordMatcher.matches()) {
-			System.out.println(inputStr + " succesfully loggedin and passed 4th rule.");
-		} else {
-			System.out.println(inputStr + " is invalid password.Kindly input minimum 8 character.");
-		}
-		return false;
-	}
-
 	public static void main(String[] args) {
-		UserRegistration psswrd = new UserRegistration();
+
+		String mailPattern = "^abc(.+)[A-Za-z0-9]{3}+(@+)bl+(.+)[co]*(.[A-Za-z]{2})$";
+		Pattern regex = Pattern.compile(mailPattern);
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please login using 8 character password having one upper case and one numeric number.");
-		System.out.print("Enter the password : ");
+		System.out.print("Enter the email ID : ");
 		String inputStr = sc.nextLine();
+		Matcher emailIDMatcher = regex.matcher(inputStr);
 		sc.close();
-		psswrd.passwrdRule(inputStr);
+		if (emailIDMatcher.matches()) {
+			System.out.println(inputStr + " is valid mail.");
+		} else {
+			System.out.println(inputStr + " is invalid mail address.Kindly input the right one.");
+		}
 	}
 }
