3e4e60fbe9a5dc9b61d6aa76ac7fc083f259af28
kumudinidesale <kdesale1211@gmail.com>
2022-03-04 13:45:37 +0530

    added:Check Regex For Email Address Logic

--- src/Java/com/blz/UserRegistration.java
+++ src/Java/com/blz/UserRegistration.java
@@ -20,18 +20,18 @@ public class UserRegistration {
 		 * @param : Name, Regex, Matches
 		 */
 
-		String mailPattern = "^abc(.+)[A-Za-z0-9]{3}+(@+)bl+(.+)[co]*(.[A-Za-z]{2})$";
-		Pattern regex = Pattern.compile(mailPattern);
+		String contactNumPattern = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern regex = Pattern.compile(contactNumPattern);
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the email ID : ");
+		System.out.print("Enter the contact number : ");
 		String inputStr = sc.nextLine();
-		Matcher emailIDMatcher = regex.matcher(inputStr);
+		Matcher contactNumMatcher = regex.matcher(inputStr);
 		sc.close();
-		if (emailIDMatcher.matches()) {
-			System.out.println(inputStr + " is valid mail.");
+		if (contactNumMatcher.matches()) {
+			System.out.println(inputStr + " is valid contact number.");
 		} else {
-			System.out.println(inputStr + " is invalid mail address.Kindly input the right one.");
+			System.out.println(inputStr + " is invalid contact number.Kindly input the right one.");
 		}
 	}
 }
